634957f78cc845726adfa016a872fdb9f7bcfdf0
Basith25 <113426014+Basith25@users.noreply.github.com>
2022-10-04 09:40:31 +0530

    Create Even numbers.c

---
+++ Even numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+void main() {
+int i;
+   printf("\nC program to print Even numbers : ");
+   for ( i = 1 ; i < 20 ; i++ )
+   {
+      i = i+1;
+      printf("%d  ", i);
+
+   }
+}
+
+Output:
+C program to print Even numbers : 2  4  6  8  10  12  14  16  18  20  
+Saving session...
+...copying shared history...
+...saving history...truncating history files...
+...completed.
+
